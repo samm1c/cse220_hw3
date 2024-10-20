f231191ddcecdc75157086115898147677ad08a7
sammi <158765072+samm1c@users.noreply.github.com>
2024-10-20 21:18:52 +0000

    p1 disable %*[^\n] and changed comment

--- src/image.c
+++ src/image.c
@@ -1,63 +1,66 @@
 #include "image.h"
 
 // part 1
-Image *load_image(char *filename) {
+    Image *load_image(char *filename) {
 
-    FILE *fp = fopen(filename, "r");
-    if (fp == NULL) { // no file found
-        return NULL;
-    }
-
-    char format[3]; // buffer for P3, +1 for null char
-    fscanf(fp, "%2s", format); // P3
-    // if (strcmp(format, "P3") != 0) { // NOT P3
-    //     fclose(fp);
-    //     return NULL;
-    // }
-
-    fscanf(fp, " %*[^\n]"); // skip extra characters and the new line
-    char line[100]; // char array buffer to store second line
-    fgets(line, sizeof(line), fp);
-    while (line[0] == '#') { // comment detected! -> skip lines
-        fscanf(fp, " %*[^\n]");
-        fgets(line, sizeof(line), fp);
-    }
-
-    unsigned int width = 0, height = 0;
-    unsigned char intensity = 0;
-    fscanf(fp, "%u %u", &width, &height);
-    fscanf(fp, " %*[^\n]");
-    fscanf(fp, " %c", &intensity);
-    
-    unsigned int **pixels = malloc(height * sizeof(unsigned int *)); // create dynamic array by malloc'ing
-    for (unsigned int i = 0; i < height; i++) { // malloc per row
-        pixels[i] = malloc(width * sizeof(unsigned int));
-    }
-    unsigned int i = 0, j = 0, k = 0; // stores R G B 
-
-    for (unsigned int p = 0; p < height; p++) { // p -> pixels array index
-        for (unsigned int q = 0; q < width; q++) {
-            fscanf(fp, "%u %u %u", &i, &j, &k);
-            pixels[p][q] = i; // still dereferences pixels; same as *(pixels + p) but syntactic sugar
-            fscanf(fp, " %*[^\n]"); // skip any spaces or newlines
+        FILE *fp = fopen(filename, "r");
+        if (fp == NULL) { // no file found
+            return NULL;
         }
-        
-    }
 
-    fclose(fp);
-    Image *img = malloc(sizeof(Image));
-    img->width = width;
-    img->height = height;
-    img->pixels = pixels;
-    return img;
+        char format[3]; // buffer for P3, +1 for null char
+        fscanf(fp, "%2s", format); // P3
+        // if (strcmp(format, "P3") != 0) { // NOT P3
+        //     fclose(fp);
+        //     return NULL;
+        // }
+
+        //fscanf(fp, " %*[^\n]"); // skip extra characters and the new line
+        
+        // skip comment(s)
+        char line[256]; // char array buffer to store second line
+        fgets(line, sizeof(line), fp);
+        while (fgets(line, sizeof(line), fp);) { // comment detected! -> skip lines
+            if (line[0] != '#') {
+                break;
+            }
+        }
+
+        unsigned int width = 0, height = 0;
+        unsigned char intensity = 0;
+        fscanf(fp, " %u %u ", &width, &height);
+        //fscanf(fp, " %*[^\n]");
+        fscanf(fp, " %c ", &intensity);
+        
+        unsigned int **pixels = malloc(height * sizeof(unsigned int *)); // create dynamic array by malloc'ing
+        for (unsigned int i = 0; i < height; i++) { // malloc per row
+            pixels[i] = malloc(width * sizeof(unsigned int));
+        }
+        unsigned int r = 0, g = 0, b = 0; // stores R G B 
+        for (unsigned int p = 0; p < height; p++) { // p -> pixels array index
+            for (unsigned int q = 0; q < width; q++) {
+                fscanf(fp, " %u %u %u ", &r, &g, &b);
+                pixels[p][q] = r; // still dereferences pixels; same as *(pixels + p) but syntactic sugar
+                //fscanf(fp, " %*[^\n]"); // skip any spaces or newlines
+            }
+        }
+
+        fclose(fp);
+
+        Image *img = malloc(sizeof(Image));
+        img->width = width;
+        img->height = height;
+        img->pixels = pixels;
+
+        return img;
 }
 
 void delete_image(Image *image) {
     if (image != NULL) { // make sure image actually exists first
-        for (unsigned int i = 0; i < (image->height); i++) {// deallocate the 2D array memory
+        for (unsigned int i = 0; i < (image->height); i++) {// deallocate each row
             free(image->pixels[i]);
         }
-        free(image->pixels); 
+        free(image->pixels); // deallocate 
         image->pixels = NULL; // pixels officially point to nothing
         free(image); // free object
     }
