37e1374d998ae6401e25d1deecd01e7453c45681
sammi <158765072+samm1c@users.noreply.github.com>
2024-10-23 18:31:52 +0000

    p3b first template reveal_image

--- src/image.c
+++ src/image.c
@@ -328,6 +328,48 @@ unsigned int hide_image(char *secret_image_filename, char *input_filename, char 
 }
 
 void reveal_image(char *input_filename, char *output_filename) {
-    (void)input_filename;
-    (void)output_filename;
+
+    FILE *f_input = fopen(input_filename, "r");
+    FILE *f_output = fopen(output_filename, "w");
+
+    // skip the first 3 or so lines + comments
+    char line[256]; // buffer -> holds current line
+    for (int i = 0; i < 3; i++) {
+        fgets(line, sizeof(line), f_input);
+        if (line[0] == '#') {
+            i--;
+        }
+    }
+
+    // read the first 8 pixels -> width
+    unsigned int secret_w = 0;
+    unsigned int r, g, b;
+    for (int i = 7; i >= 0; i--) {
+        fscanf(f_input, "%u %u %u ", &r, &g, &b);
+        unsigned int k = r & 1; // k -> last bit
+        secret_w |= (k << i); // add the bit by pushing it into position
+    }
+
+    // read the next 8 pixels -> height
+    unsigned int secret_h = 0;
+    for (int i = 7; i >= 0; i--) {
+        fscanf(f_input, "%u %u %u ", &r, &g, &b);
+        unsigned int k = r & 1;
+        secret_h |= (k << i);
+    }
+
+    // build the output file using the information you just found
+    fprintf(f_output, "P3\n%u %u\n255\n", secret_w, secret_h);
+
+    // using the secret w and h you just found, iterate w*h times to obtain the last bit of each pixel for intensity; 8 pixels -> 1 intensity
+    for (unsigned int i = 0; i < (secret_w * secret_h); i++) {
+        unsigned int intensity = 0; // stores intensity that we are building
+        for (int j = 7; j >= 0; j--) {
+            fscanf(f_input, "%u %u %u ", &r, &g, &b);
+            unsigned int k = r & 1;
+            intensity |= (k << j);
+        }
+        printf("intensity: %u\n", intensity);
+        fprintf(f_output, "%u %u %u\n", intensity, intensity, intensity);
+    }
 }
